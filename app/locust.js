8681b0a9108f39e3eeb93d6fbabaab7b4be0a693
Gianni Carafa <gianni.carafa@srf.ch>
2021-09-11 22:32:23 +0200

    start also when kubernetes is not accessible

--- app/locust.js
+++ app/locust.js
@@ -9,33 +9,37 @@ let locust = {
 
 // initial loading of all instances
 async function init() {
-  debug('loading');
-  const result = await kubectl.list('ppp');
-    result.services.response.body.items.forEach(element => {
-        locust.instances[element.metadata.name] = {
-            name: element.metadata.name,
-            namespace: element.metadata.namespace,
-            creationTimestamp: element.metadata.creationTimestamp,
-        };
-    });
+    try {
+        debug('loading');
+        const result = await kubectl.list('ppp');
+        result.services.response.body.items.forEach(element => {
+            locust.instances[element.metadata.name] = {
+                name: element.metadata.name,
+                namespace: element.metadata.namespace,
+                creationTimestamp: element.metadata.creationTimestamp,
+            };
+        });
 
-    result.ingresses.response.body.items.forEach(element => {
-        locust.instances[element.metadata.name.replace("-ingress", "")]['ingressHost'] = element.spec.rules[0].host;
-    });
-    result.deployments.response.body.items.forEach(element => {
-        if (element.metadata.name.endsWith("-worker")) {
-            locust.instances[element.metadata.name.replace("-worker", "")]['worker'] = element.spec.replicas;
-        }
-        if (element.metadata.name.endsWith("-master")) {
-            masterinstance = element.metadata.name.replace("-master", "")
-            locust.instances[masterinstance]['testHost'] = element.spec.template.spec.containers[0].env[0].value;
-            locust.instances[masterinstance]['numUsers'] = element.spec.template.spec.containers[0].env[1].value;
-            locust.instances[masterinstance]['spawnRate'] = element.spec.template.spec.containers[0].env[2].value;
-            locust.instances[masterinstance]['locustfile'] = element.spec.template.spec.volumes[0].configMap.name;
-        }
-    });
-    console.log(locust);
-    debug('loaded');
+        result.ingresses.response.body.items.forEach(element => {
+            locust.instances[element.metadata.name.replace("-ingress", "")]['ingressHost'] = element.spec.rules[0].host;
+        });
+        result.deployments.response.body.items.forEach(element => {
+            if (element.metadata.name.endsWith("-worker")) {
+                locust.instances[element.metadata.name.replace("-worker", "")]['worker'] = element.spec.replicas;
+            }
+            if (element.metadata.name.endsWith("-master")) {
+                masterinstance = element.metadata.name.replace("-master", "")
+                locust.instances[masterinstance]['testHost'] = element.spec.template.spec.containers[0].env[0].value;
+                locust.instances[masterinstance]['numUsers'] = element.spec.template.spec.containers[0].env[1].value;
+                locust.instances[masterinstance]['spawnRate'] = element.spec.template.spec.containers[0].env[2].value;
+                locust.instances[masterinstance]['locustfile'] = element.spec.template.spec.volumes[0].configMap.name;
+            }
+        });
+        console.log(locust);
+        debug('loaded');
+    } catch (error) {
+        console.log("Cant connect to Kubernetes Cluster");
+    }
 }
 
 async function addLocust(namespace, instance, locustfile, hostname, workers, testHost, numUsers, spawnRate) {
